7c689374832ea74c485b4c081e825b493da35f4f
CRJETHII <91737447+CRJETHII@users.noreply.github.com>
2023-05-09 00:21:14 +0530

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"What's your age?\n";
+    cin>>a;
+    cout<<"your age is "<<a<<endl;
+    return 0;
+}
---
+++ decimal_to_binary.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int DecimalToBinary(int n){
+    int x = 1;
+    int ans = 0;
+    while(x<=n){
+        x *= 2;
+        }
+        x/=2;
+
+    while(x>0)
+    {
+        int LastDigit = n/x;
+        n -= LastDigit*x;
+        x /= 2;
+        ans = ans*10 + LastDigit;
+    }
+    return ans;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    cout<<DecimalToBinary(n)<<endl;
+
+}
