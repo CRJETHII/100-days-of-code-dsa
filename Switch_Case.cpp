e4ea7d5e48e2f13f0491e788b30354d85662952e
CRJETHII <91737447+CRJETHII@users.noreply.github.com>
2023-06-06 19:28:05 +0530

    Add files via upload

---
+++ Switch_Case.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int Rs100, Rs50, Rs20, Rs1;
+    cout<<"total amount is Rs"<<n<<endl;
+
+    switch(1){
+        case 1: Rs100=n/100;
+        n=n%100;
+        cout<<"No of 100 Rupee Notes "<<Rs100<<endl;
+
+        case 2: Rs50=n/50;
+        n=n%50;
+        cout<<"No of 50 Rupee Notes is "<<Rs50<<endl;
+
+        case 3: Rs20=n/20;
+        n=n%20;
+        cout<<"No of 20 Rupee Notes is "<<Rs20<<endl;
+
+        case 4: Rs1=n/1;
+        n=n%1;
+        cout<<"No of 1 rupee notes is "<<Rs1<<endl;
+    }
+    return 0;
+}
